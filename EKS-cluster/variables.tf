beb9da453d5b2946fc6bc4de022a25461d8bd32d
paulopkl <palmeida.ipms@gmail.com>
2024-12-09 23:37:27 -0300

    feat: repo initialized

--- EKS-cluster/variables.tf
+++ EKS-cluster/variables.tf
@@ -1,36 +1,44 @@
 variable "vpc_cidr_block" {
   type    = string
-  default = ""
+  default = "10.0.0.0/16"
 }
 
 variable "private_subnet_cidr_blocks" {
-  type    = set(string)
-  default = [""]
+  type = set(string)
+  default = [
+    "10.0.4.0/24",
+    "10.0.5.0/24",
+    "10.0.6.0/24"
+  ]
 }
 
 variable "public_subnet_cidr_blocks" {
-  type    = set(string)
-  default = [""]
+  type = set(string)
+  default = [
+    "10.0.7.0/24",
+    "10.0.8.0/24",
+    "10.0.9.0/24"
+  ]
 }
 
 variable "cluster_name" {
   type    = string
-  default = ""
+  default = "test-jenkins"
 }
 
 variable "vpc_name" {
   type    = string
-  default = ""
+  default = "eks-vpc"
 }
 
 variable "bucket_name" {
   type        = string
   description = "AWS Bucket for backend provider"
-  default     = ""
+  default     = "paulopkl-jenkins-terraform-kubernetes"
 }
 
 variable "bucket_region" {
   type        = string
   description = "AWS Bucket region for backend provider"
-  default     = ""
+  default     = "us-west-1"
 }
